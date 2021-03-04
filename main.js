cd9925922c64194fb2ec6cd62a4127e141cf0216
Patryk Adach <patrykadachofficial@gmail.com>
2021-03-04 16:05:37 +0100

    Added async / await / fetch

--- main.js
+++ main.js
@@ -34,3 +34,38 @@ function createPost(post) {
 createPost({ title: 'post three', body: 'this is post three'})
 	.then(getPosts)
 		.catch(err => alert(err));
+
+
+// Promise.all
+const promise1 = Promise.resolve('hello world');
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject) => setTimeout(resolve, 2000, 'Goodbye'));
+
+const promise4 = fetch('https://jsonplaceholder.typicode.com/users')
+	.then(res => res.json()); 
+
+Promise.all([promise1, promise2, promise3]).then((values) => console.log(values));
+
+async function init() {
+	await createPost({ title: 'post three', body: 'this is post three'});
+	getPosts();
+}
+
+init();
+
+// Async / Await / Fetch
+async function fetchUsers() {
+	const res = await fetch('https://jsonplaceholder.typicode.com/users');
+	const data = await res.json();
+	console.log(data);
+}
+
+fetchUsers();
+
+
+
+
+
+
+
+
